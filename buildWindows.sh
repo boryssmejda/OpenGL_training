47ed95adc857fa124c0cfd1e1ecd3767c7c36ca2
borys <borys.smejda@gmail.com>
2020-04-22 17:31:39 +0200

    Windows build changed

--- buildWindows.sh
+++ buildWindows.sh
@@ -2,6 +2,6 @@ mkdir build
 cd build
 conan install .. -s build_type=Release
 conan install .. -s build_type=Debug
-cmake .. -G "Visual Studio 16 2019 Win64"
+cmake .. -G "Visual Studio 16 2019"
 cmake --build . --config Release
 cmake --build . --config Debug
