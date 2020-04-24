e186bda23493dd7752f7f7288137aad56428827f
borys <borys.smejda@gmail.com>
2020-04-24 14:10:37 +0200

    Starting with code, replaced raw ptr with unique_ptr

--- main.cpp
+++ main.cpp
@@ -1,27 +1,33 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <memory>
+#include <iostream>
 
-int main(void)
+int main()
 {
-    GLFWwindow* window;
-
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    auto windowDeleter = [](GLFWwindow* w) {
+        std::cout << "Closing the window!\n";
+        glfwTerminate();
+    };
+    std::unique_ptr<GLFWwindow, decltype(windowDeleter)> glfwWindow(
+        glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr), windowDeleter);
+
+    if (!glfwWindow)
     {
         glfwTerminate();
         return -1;
     }
 
     /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(glfwWindow.get());
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(glfwWindow.get()))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
@@ -34,12 +40,11 @@ int main(void)
         glEnd();
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(glfwWindow.get());
 
         /* Poll for and process events */
         glfwPollEvents();
     }
 
-    glfwTerminate();
     return 0;
 }
